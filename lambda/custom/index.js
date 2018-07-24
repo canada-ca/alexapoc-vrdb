675faa68ced44e943ded6e92117474d323e5f871
Ainsley <ains_bernard@hotmail.com>
2018-07-24 14:45:15 -0400

    fixed SearchByVehicle Intent

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -26,82 +26,148 @@ const InProgressSearchByVehicleIntentHandler = {
   canHandle(handlerInput) {
     return handlerInput.requestEnvelope.request.type === 'IntentRequest'
       && handlerInput.requestEnvelope.request.intent.name === 'SearchByVehicle';
-      //&& request.dialogState !== 'COMPLETED';
+      // && request.dialogState !== 'COMPLETED';
 
   },
   handle(handlerInput) {
     const currentIntent = handlerInput.requestEnvelope.request.intent;
     let prompt = '';
+    var speechText = 'Hello World!';
+
+  
+    
+
 
     for (const slotName in currentIntent.slots) {
+      speechText+= currentIntent.slots[slotName].value;
+      console.log(slotName)
+
+
+      const currentSlot = currentIntent.slots[slotName];
+        
+      if(!currentIntent.slots[slotName].value)
+      {
+        prompt = `What ${currentSlot.name} is it?`;
+
+        return handlerInput.responseBuilder
+          .speak(prompt)
+          .reprompt(prompt)
+          .addElicitSlotDirective(currentSlot.name)
+          .getResponse();
+      }
+      console.log(currentSlot);
       if (Object.prototype.hasOwnProperty.call(currentIntent.slots, slotName)) {
-        const currentSlot = currentIntent.slots[slotName];
-        if (currentSlot.confirmationStatus !== 'CONFIRMED'
-          && currentSlot.resolutions
-          && currentSlot.resolutions.resolutionsPerAuthority[0]) {
-          if (currentSlot.resolutions.resolutionsPerAuthority[0].status.code === 'ER_SUCCESS_MATCH') {
-            if (currentSlot.resolutions.resolutionsPerAuthority[0].values.length > 1) {
-              prompt = 'Which would you like';
-              const size = currentSlot.resolutions.resolutionsPerAuthority[0].values.length;
+  
+        // console.log(currentSlot.resolutions );
+        // console.log(currentSlot.resolutions.resolutionsPerAuthority[0]);
+        // console.log(currentSlot.resolutions.resolutionsPerAuthority[0].status.code );
+        // console.log(currentSlot.resolutions.resolutionsPerAuthority[0].values.length  );
+        // console.log(currentSlot.confirmationStatus );
 
-              currentSlot.resolutions.resolutionsPerAuthority[0].values
-                .forEach((element, index) => {
-                  prompt += ` ${(index === size - 1) ? ' or' : ' '} ${element.value.name}`;
-                });
 
-              prompt += '?';
 
-              return handlerInput.responseBuilder
-                .speak(prompt)
-                .reprompt(prompt)
-                .addElicitSlotDirective(currentSlot.name)
-                .getResponse();
-            }
-          } else if (currentSlot.resolutions.resolutionsPerAuthority[0].status.code === 'ER_SUCCESS_NO_MATCH') {
-            if (requiredSlots.indexOf(currentSlot.name) > -1) {
-              prompt = `What ${currentSlot.name} are you looking for`;
 
-              return handlerInput.responseBuilder
-                .speak(prompt)
-                .reprompt(prompt)
-                .addElicitSlotDirective(currentSlot.name)
-                .getResponse();
-            }
-          }
-        }
+        // // // // if (currentSlot.confirmationStatus !== 'CONFIRMED'){
+        // // // //   // && currentSlot.resolutions
+        // // // //   // && currentSlot.resolutions.resolutionsPerAuthority[0]) {
+        // // // //   // if (currentSlot.resolutions.resolutionsPerAuthority[0].status.code === 'ER_SUCCESS_MATCH') {
+        // // // //     // if (currentSlot.resolutions.resolutionsPerAuthority[0].values.length > 1) {
+        // // // //       prompt = 'Which would you like';
+        // // // //       const size = currentSlot.resolutions.resolutionsPerAuthority[0].values.length;
+        // // // //       speechText = "inside most inner if statement"
+
+        // // // //       currentSlot.resolutions.resolutionsPerAuthority[0].values
+        // // // //         .forEach((element, index) => {
+        // // // //           prompt += ` ${(index === size - 1) ? ' or' : ' '} ${element.value.name}`;
+        // // // //         });
+
+        // // // //       prompt += '?';
+
+        // // // //       return handlerInput.responseBuilder
+        // // // //         .speak(prompt)
+        // // // //         .reprompt(prompt)
+        // // // //         .addElicitSlotDirective(currentSlot.name)
+        // // // //         .getResponse();
+        // // // //     // }
+        // // // //   } else if (currentSlot.resolutions.resolutionsPerAuthority[0].status.code === 'ER_SUCCESS_NO_MATCH') {
+        // // // //     if (requiredSlots.indexOf(currentSlot.name) > -1) {
+        // // // //       prompt = `What ${currentSlot.name} are you looking for`;
+
+        // // // //       return handlerInput.responseBuilder
+        // // // //         .speak(prompt)
+        // // // //         .reprompt(prompt)
+        // // // //         .addElicitSlotDirective(currentSlot.name)
+        // // // //         .getResponse();
+        // // // //     }
+        // // // //   }
+        // // // // }
       }
     }
-
-    return handlerInput.responseBuilder
-      .addDelegateDirective(currentIntent)
-      .getResponse();
-  },
-};
-
-
-const SearchByVehicleIntentHandler = {
-  canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-      && handlerInput.requestEnvelope.request.intent.name === 'SearchByVehicle'
-      && request.dialogState === 'COMPLETED';
-
-  },
-  handle(handlerInput) {
-    const speechText = 'Hello World!';
-
-        console.log("inside vehicle handler");
-        // // delegate to Alexa to collect all the required slots
-        // let isTestingWithSimulator = true; //autofill slots when 
-        // //using simulator, dialog management is only supported with a device
-        //let filledSlots = delegateSlotCollection.call(this);
-        
 
     return handlerInput.responseBuilder
       .speak(speechText)
       .withSimpleCard('Hello World', speechText)
       .getResponse();
-  },
+
+    // return handlerInput.responseBuilder
+    //   .addDelegateDirective(currentIntent)
+    //   .getResponse();
+  }
 };
+
+
+const SearchByVehicleIntentHandler = {
+  canHandle(handlerInput) {
+    console.log("inside search vehicle intent handler")
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'SearchByVehicle';
+  },
+  handle(handlerInput) {
+    const speechText = 'Hello World!';
+
+        // console.log("inside vehicle handler");
+        // // // delegate to Alexa to collect all the required slots
+        // // let isTestingWithSimulator = true; //autofill slots when 
+        // // //using simulator, dialog management is only supported with a device
+        // let filledSlots = delegateSlotCollection.call(this);
+        
+
+    
+        // if (!filledSlots) {
+        //     return;
+        // }
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .withSimpleCard('Hello World', speechText)
+      .getResponse();
+  }
+};
+
+
+// function delegateSlotCollection() {
+//   console.log("inside delegate slot")
+//   let updatedIntent = this.event.request.intent.dialogState;
+//   if (this.event.request.intent.dialogState === "STARTED") {
+//       this.emit(':delegate', updatedIntent);
+//   } else if (this.event.request.intent.dialogState !== "COMPLETED") {
+//       if(this.event.request.intent.slots.purpose.value) {
+//           let prompt = "We have 3 gaming computers on the shelf. ";
+//           prompt +=  "Two premium computers and one cheap one. Which would you like?";
+//           let reprompt = "In stock, we have premium and cheap computers, which would you like?";
+          
+//           this.emit(':elicitSlot', 'budget', prompt, reprompt);                
+//       } else {
+//           this.emit(':delegate', updatedIntent);
+//       }
+//   } else {
+//       // we have collected all of our slots! 
+//       // time to return them
+//       return updatedIntent 
+//   }
+//   return null;
+// }
+
 
 const HelpIntentHandler = {
   canHandle(handlerInput) {
@@ -165,11 +231,11 @@ const skillBuilder = Alexa.SkillBuilders.custom();
 exports.handler = skillBuilder
   .addRequestHandlers(
     LaunchRequestHandler,
-    SearchByVehicleIntentHandler,
+    // SearchByVehicleIntentHandler,
     HelpIntentHandler,
     CancelAndStopIntentHandler,
     SessionEndedRequestHandler,
-    InProgressSearchByVehicleIntentHandler
+    InProgressSearchByVehicleIntentHandler 
   )
   .addErrorHandlers(ErrorHandler)
   .lambda();
